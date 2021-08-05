410644d40ff80e7bd80b7453458ea083f494823d
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-08-04 19:34:04 -0600

    Update reverseLL.js
    added sol 2

--- reverseLL.js
+++ reverseLL.js
@@ -7,3 +7,20 @@ head.next.next = head;
 head.next = undefined;
 return tmp;
 }
+
+
+
+///or sol 2:
+
+const reverse = (head) => {
+   if (!head || !head.next) {
+     return head;
+   }
+   let temp = reverse(head.next);
+   head.next.next = head;
+   head.next = undefined;
+   return temp;
+ }    
+
+
+//using recursion again , When reverse() reaches the end of the list, it will grab the last node as the new head and reference each node backwards.
